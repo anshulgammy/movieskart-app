869f67f458d8ca2437dd7db73c205174b85dcc65
anshulgammy <anshulgammy@gmail.com>
2022-03-18 16:57:37 +0530

    spotless applied

--- movieskart-discovery-service/src/main/java/dev/bumbler/movieskart/discovery/MoviesKartDiscoveryServiceApplication.java
+++ movieskart-discovery-service/src/main/java/dev/bumbler/movieskart/discovery/MoviesKartDiscoveryServiceApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class MoviesKartDiscoveryServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MoviesKartDiscoveryServiceApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(MoviesKartDiscoveryServiceApplication.class, args);
+  }
 }
