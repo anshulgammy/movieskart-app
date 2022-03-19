090a9b3dd70ba92eca9f1fd6bb96f023c7250295
anshulgammy <anshulgammy@gmail.com>
2022-03-19 17:31:29 +0530

    Added Spring Cloud Config Server Support

--- movieskart-config-service/src/main/java/dev/bumbler/movieskart/config/MoviesKartConfigServiceApplication.java
+++ movieskart-config-service/src/main/java/dev/bumbler/movieskart/config/MoviesKartConfigServiceApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 public class MoviesKartConfigServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MoviesKartConfigServiceApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(MoviesKartConfigServiceApplication.class, args);
+  }
 }
